a70bb7c0a9114fc696a143984387fafa0e6f5ce0
weikangg <chongweikang5@gmail.com>
2025-04-13 00:38:18 +0800

    test new yaml

--- frontend_mobile/FitnessApp/services/config.ts
+++ frontend_mobile/FitnessApp/services/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_DOMAIN_URL = "http://54.254.95.165:8000";
+export const BACKEND_DOMAIN_URL = "http://54.179.241.8:8000";
 export const BACKEND_API_URL = `${BACKEND_DOMAIN_URL}/optimize`;
